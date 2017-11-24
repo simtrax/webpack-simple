b5e9cece0824545ec4aca15cfa388d895983da05
Simon Månsson <simon.mansson@niftitech.com>
2017-11-24 14:19:41 +0100

    Required the Leaflet module

--- template/src/main.js
+++ template/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/** Require in the leaflet module from the folder node_modules done automagically */
+window.L = require('leaflet');
+
+/** Start the vue app and bind it to the app element */
 new Vue({
   el: '#app',
   render: h => h(App)
